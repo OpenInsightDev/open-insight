fa9a63178199e788a5b8e0abf8329f3ef9bceb04
王博伟 <wozluohd@gmail.com>
2026-07-05 16:58:17 +0800

    backup

--- packages/rl/vite.config.ts
+++ packages/rl/vite.config.ts
@@ -1,17 +1,31 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   pack: {
     dts: {
       tsgo: true,
     },
-    exports: true,
+    clean: true,
+    sourcemap: true,
+    entry: {
+      internal: "src/index.ts",
+      index: "src/export.ts",
+    },
+    exports: {
+      devExports: true,
+    },
   },
   lint: {
     options: {
       typeAware: true,
       typeCheck: true,
     },
+  },
+  test: {
+    include: ["src/**/*.test.ts", "tests/**/*.test.ts"],
   },
   fmt: {},
 });
