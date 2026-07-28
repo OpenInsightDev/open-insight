e80401f6035ac7f46ffe7b8a50d51131b75c0d4e
王博伟 <wozluohd@gmail.com>
2026-07-28 22:22:02 +0800

    fix dashboard theme children

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -2,7 +2,7 @@ import { Theme } from "@astryxdesign/core/theme";
 import { neutralTheme } from "@astryxdesign/theme-neutral/built";
 
 function App() {
-  return <Theme theme={neutralTheme}></Theme>;
+  return <Theme theme={neutralTheme}>{null}</Theme>;
 }
 
 export default App;
