5a1731d945d24cc146d0a42330b07dc51428fb14
王博伟 <wangbowei@nudt.edu.cn>
2026-09-10 18:03:42 +0800

    backup

--- packages/core/src/sandbox/export.ts
+++ packages/core/src/sandbox/export.ts
@@ -1,14 +1,4 @@
 export * from "./error.ts";
-export * from "./builtin/export.ts";
-export {
-  Sandbox,
-  Terminal,
-  layer,
-  type QuitError,
-  type TerminalService,
-  type UserInput,
-} from "./sandbox.ts";
-export { QuitError, UserInput } from "effect/Terminal";
 export { type Provider, ProviderService } from "./provider.ts";
 
 export * as FileSystem from "./fs.ts";
